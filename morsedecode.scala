b1deb198281e6fc88e55cb4ca07c6ddaaf4af799
yaxis1 <vercettisaif@outlook.com>
2021-03-23 05:21:00 +0100

    Decoding the morse code

--- morsedecode.scala
+++ morsedecode.scala
@@ -1,5 +1,3 @@
-```scala
-
 object MorseDecoding {
 
     // From wikipedia - mapping each char to its corresponding morse.
@@ -13,27 +11,15 @@ object MorseDecoding {
     '4' -> "....-", '5' -> ".....", '6' -> "-....", '7' -> "--...", '8' -> "---..",
     '9' -> "----."
     )
-```
   
-```scala
-
     // This function takes a string and returns the corresponding character as mapped in morse val.
     def morseDecode(s: String) = {
-  
+        
         printf(morse.filter(_._2 == s).map(_._1).mkString(" "))
     }
-```
-
-```scala
 
     def main(args: Array[String]) = {
         args.map(morseDecode)
         println(" ")
     }
 }
-```
-
-![exercise2](https://user-images.githubusercontent.com/38083799/112092286-7be72300-8b97-11eb-91a7-1777867ee69d.png)
-
-
-
